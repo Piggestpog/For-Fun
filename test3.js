003ea1a7b6e4ac6c686fa6fde08e78894b3a66d9
Piggestpog <81506494+Piggestpog@users.noreply.github.com>
2022-05-27 16:30:45 +0800

    Update test3.js

--- test3.js
+++ test3.js
@@ -1,11 +1,11 @@
 
 let obj=JSON.parse($response.body);
 obj = {
-    "account": [{
+    "account": {
     "pro_mode": true,
     "plan_type": "Premium",
     "user_type": "Premium",
-    }]
+    }
 };
 
 $done({body:JSON.stringify(obj)});
