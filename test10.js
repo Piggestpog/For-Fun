d2414fba2491830895d6d12d1ae66c04606e7ba6
Piggestpog <81506494+Piggestpog@users.noreply.github.com>
2022-05-27 17:00:49 +0800

    Update test10.js

--- test10.js
+++ test10.js
@@ -1,11 +1,6 @@
-let obj = JSON.parse($response.body)
-obj = {
-  "account": {
-    "pro_mode": true,
-    "plan_type": "Premium",
-    "user_type": "Premium",
-    "current_period": "2099-10-10T03:27:34"
-  }
-}
-
+let obj=JSON.parse($response.body)
+obj.account["pro_mode"] = true;
+obj.account["plan_type"] = "Premium";
+obj.account["user_type"] = "Premium";
+obj.account["current_period"] = "2099-10-10T03:27:34";
 $done({body:JSON.stringify(obj)})
